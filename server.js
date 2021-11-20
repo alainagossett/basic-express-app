bfd8c63b19120f061961ec21cd71052e4629f7a4
Alaina <alaina.gossett@gmail.com>
2021-11-20 14:46:38 -0700

    add tweeter routes

--- server.js
+++ server.js
@@ -32,8 +32,45 @@ app.use(methodOverride("_method"))
 //Routes
 
 //Index
-app.get('/', (req, res) => {
-    res.send("Hello World!")
+app.get('/tweets', (req, res) => {
+    Tweet.find({}, (err, arrayOfTweets) => {
+        res.send(arrayOfTweets);
+    }) //empty query object "{}" to look for all tweets
+})
+
+//New route
+
+//Delete route
+//put id in the param so we can specify which document we want to delete
+app.delete('/tweets/:id', (req, res) => {
+    Tweet.findByIdAndDelete(req.params.id, (err, copyOfDeletedTweet) => {
+        res.send(copyOfDeletedTweet);
+    })
+})
+
+//Update route
+//put id in param so we can specify which document we want to update
+app.put('/tweets/:id', (req, res) => {
+    Tweet.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedTweet) => {
+        res.send(updatedTweet);
+    })
+})
+
+//Create route
+app.post('/tweets', (req, res) => {
+    Tweet.create(req.body, (err, createdTweet) => {
+        res.send(createdTweet);
+    })//this code runs aynchronously
+})
+
+//Edit route
+
+//Show route
+//search by a specific document id
+app.get('/tweets/:id', (req, res) => {
+    Tweet.findById(req.params.id, (err, foundTweet) => {
+        res.send(foundTweet);
+    })
 })
 
 
